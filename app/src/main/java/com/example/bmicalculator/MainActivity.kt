337731daf529d6842ac157dffbb1adf5d6ef4870
Arittrabs1528 <arittra.das@brainstation-23.com>
2024-05-06 12:17:04 +0600

    Bug fixed

--- app/src/main/java/com/example/bmicalculator/MainActivity.kt
+++ app/src/main/java/com/example/bmicalculator/MainActivity.kt
@@ -29,23 +29,23 @@ class MainActivity : AppCompatActivity() {
             insets
         }
     }
-    private fun calculateBMI(){
+
+    private fun calculateBMI() {
         val weight = binding.weightEdit.text.toString().toFloatOrNull()
         val height = binding.heightEdit.text.toString().toFloatOrNull()
 
-        if(weight != null && height != null){
-            val bmi = weight/ (height/100).pow(2)
-            val bmiResult = String.format("%.2f",bmi)
+        if (weight != null && height != null) {
+            val bmi = weight / (height / 100).pow(2)
+            val bmiResult = String.format("%.2f", bmi)
 
-            val bmiCategory = when{
+            val bmiCategory = when {
                 bmi < 18.5 -> "Underweight"
-                bmi <25 -> "Normal weight"
-                bmi <30 -> "Overweight"
+                bmi < 25 -> "Normal weight"
+                bmi < 30 -> "Overweight"
                 else -> "Obese"
             }
             binding.resultText.text = "BMI: $bmiResult\nCategory: $bmiCategory"
-        }
-        else{
+        } else {
             binding.resultText.text = "Invalid Input"
         }
     }
